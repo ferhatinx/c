8ac201c75b5965c3b1c894ed9f80003bdd7e8d28
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2021-12-23 17:24:55 +0300

    operatorler

---
+++ operator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           //atama ve işlemli atama
+           int x=3;
+           int y=4;
+           int z=x+y;
+           Console.WriteLine("z nin değer "+z);
+
+           z+=4;
+           Console.WriteLine("z nin yeni değeri"+z);
+           //mantıksal operaotrler
+           // || veya && ve ! değil
+           bool isSucces=true;
+           bool isCompleted=false;
+
+
+        if(isSucces && isCompleted)
+            Console.WriteLine("çok iyi");
+        //ilişkisel ifadeler
+        //<, >, >=, ==, !=
+
+        int a =3;
+        int b =4;
+
+        if(a<b)
+            Console.WriteLine("küçük");
+        else
+            Console.WriteLine("büyük");
+
+        //ilişkisel operator
+        // *, /i &,
+
+        int g=4;
+        int s=2;
+        int sonuc=a&b;
+        Console.WriteLine("sonuc"+sonuc);
+        // mod alma
+        // %
+
+        
+        }
+
+    }
+}
